382423cc955e82344afe0fe668de4767ce5bb982
Arya <aryasanj2000@gmail.com>
2024-03-31 14:26:56 -0700

    adding canvas.edu pop up

--- tabs/background.js
+++ tabs/background.js
@@ -1,8 +1,5 @@
 //using https://developer.chrome.com/docs/extensions/reference/api/tabs
-chrome.runtime.onInstalled.addListener(({reason}) => {
-    if (reason === 'install') {
-      chrome.tabs.create({
-        url: "https://canvas.uw.edu/"
-      });
-    }
-  });
+
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.create({'url':"chrome://canvas.uw.edu"})
+})
