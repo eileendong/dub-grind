384d7f4841f00d2c7c7c89570eeaa47cda1b863a
Arya <aryasanj2000@gmail.com>
2024-03-31 14:36:26 -0700

    added popup.js file

---
+++ hello-world/popup.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var moreInfoBtn = document.getElementById('more-info');
+    var menuBtn = document.getElementById('menu');
+    var yesBtn = document.getElementById('yes');
+    var maybeLaterBtn = document.getElementById('maybe-later');
+
+    moreInfoBtn.addEventListener('click', function () {
+        // Handle more info button click
+        // You can perform any action here, such as opening a new tab or window
+    });
+
+    menuBtn.addEventListener('click', function () {
+        // Handle menu button click
+        // You can perform any action here, such as opening a menu
+    });
+
+    yesBtn.addEventListener('click', function () {
+        // Handle Yes button click
+        // You can perform any action here, such as starting a study session
+    });
+
+    maybeLaterBtn.addEventListener('click', function () {
+        // Handle Maybe Later button click
+        // You can perform any action here, such as postponing the study session
+    });
+});
--- tabs/background.js
+++ tabs/background.js
@@ -1,5 +1,5 @@
 //using https://developer.chrome.com/docs/extensions/reference/api/tabs
 
-// chrome.browserAction.onClicked.addListener(function() {
-//     chrome.tabs.create({'url':"chrome://canvas.uw.edu"})
-// })
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.update(tab.id, { url: "https://canvas.uw.edu/" });
+});
